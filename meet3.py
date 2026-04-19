d4266a5d66b4a84834f590cfc9c1577e9bba28a7
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2026-04-19 17:10:05 +0800

    meet3.py
    while true,if,else

---
+++ meet3.py
@@ -0,0 +1,13 @@
+#("NAMA :kevin christian alexander")
+#("NIM  :25241088")
+#("KELAS:PTI C")
+
+while True:
+    angka = int(input("Masukkan angka: "))
+    if angka < 0:
+        print("\033[91mHarus positif!\033[0m")
+
+    
+    else:
+        print("Angka yang dimasukkan:", angka)
+        break
