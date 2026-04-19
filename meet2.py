3be64cc98243f470fa708ee046705eefca769776
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2026-04-19 16:52:17 +0800

    meet2.py
    perulangan (loop)

---
+++ meet2.py
@@ -0,0 +1,17 @@
+#("NAMA :kevin christian alexander")
+#("NIM  :25241088")
+#("KELAS:PTI C")
+
+angka = [0,1,2,3,4,5] # ini menggukan list
+
+for i in angka : 
+    print(f"nilai i = {i}")
+    print("kevin")
+print("program selesai")
+
+angka = range (1,50) #ini menggunakan range
+
+for i in angka :
+    print (f"nilaii i = {i}")
+    print("kevin")
+print("program selesai")
