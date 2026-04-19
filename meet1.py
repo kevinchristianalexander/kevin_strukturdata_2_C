598b62906a37ddd2e19887881c0d440dde19bf07
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2026-04-19 16:29:18 +0800

    mee1.py
    if else

---
+++ meet1.py
@@ -0,0 +1,13 @@
+#("NAMA :kevin christian alexander")
+#("NIM  :25241088")
+#("KELAS:PTI C")
+
+nama = input("masukan nama anda :")
+if nama=="kevin christian alexander" :
+    print("selamat datang kevin NIM anda adalah")
+    print("25241088")
+
+else :
+    print("nama salah")
+    print("anda tidak terdaftar")
+print("program selesai")
