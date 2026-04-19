e63c8ab6ee5e449b3cf9d4edb162e8fddd4e8171
kevinchristianalexander <kevinchristianalexander7@gmail.com>
2026-04-19 16:43:37 +0800

    meet2.py
    perulangan (loop)

---
+++ meet2.py
@@ -0,0 +1,16 @@
+# perulangan (loop)
+
+
+angka = [0,1,2,3,4,5] # ini menggukan list
+
+for i in angka : 
+    print(f"nilai i = {i}")
+    print("kevin")
+print("program selesai")
+
+angka = range (1,50) #ini menggunakan range
+
+for i in angka :
+    print (f"nilaii i = {i}")
+    print("kevin")
+print("program selesai")
